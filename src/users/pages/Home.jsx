a09b352b48338a80b08ee4d22e2c3f6af849545f
Athulya-2524 <athulyasubash02@gmail.com>
2025-11-26 12:59:36 +0530

    login created

--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../../components/Footer'
 import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
@@ -25,9 +26,73 @@ function Home() {
                 <h1 className="text-3xl font0bold">NEW ARRIVALS</h1>
                 <h2 className="text-2xl">Explore Our Latest Collections</h2>
                 {/* books row & col */}
+                <div className="md:grid grid-cols-4 w-full mt-10">
+                    {/* duplicate book card div */}
+                    <div className="shadow rounded p-3 mx-4 mb-5 md:mb-0">
+                        <img width={'300px'} height={'300px'} src="https://cdn2.penguin.com.au/covers/original/9781786330895.jpg" alt="book" />
+                        <div className="flex justify-center items-center flex-col mt-4">
+                            <h3 className='text-blue-600 font-bold text-lg'>Author</h3>
+                            <h4 className='text-lg'>title</h4>
+                            <h4>$ price</h4>
+                        </div>
+                    </div>
+                    <div className="shadow rounded p-3 mx-4">
+                        <img width={'300px'} height={'300px'} src="https://cdn2.penguin.com.au/covers/original/9781786330895.jpg" alt="book" />
+                        <div className="flex justify-center items-center flex-col mt-4">
+                            <h3 className='text-blue-600 font-bold text-lg'>Author</h3>
+                            <h4 className='text-lg'>title</h4>
+                            <h4>$ price</h4>
+                        </div>
+                    </div>
+                    <div className="shadow rounded p-3 mx-4">
+                        <img width={'300px'} height={'300px'} src="https://cdn2.penguin.com.au/covers/original/9781786330895.jpg" alt="book" />
+                        <div className="flex justify-center items-center flex-col mt-4">
+                            <h3 className='text-blue-600 font-bold text-lg'>Author</h3>
+                            <h4 className='text-lg'>title</h4>
+                            <h4>$ price</h4>
+                        </div>
+                    </div>
+                    <div className="shadow rounded p-3 mx-4">
+                        <img width={'300px'} height={'300px'} src="https://cdn2.penguin.com.au/covers/original/9781786330895.jpg" alt="book" />
+                        <div className="flex justify-center items-center flex-col mt-4">
+                            <h3 className='text-blue-600 font-bold text-lg'>Author</h3>
+                            <h4 className='text-lg'>title</h4>
+                            <h4>$ price</h4>
+                        </div>
+                    </div>
+                </div>
+                {/* all books link */}
+                <div className="text-center mt-20">
+                    <Link to={'/books'} className='p-3 bg-black text-white'>Explore more...</Link>
+                </div>
             </section>
             {/* author */}
+            <section className='md:px-40 p-5 my-5 md:grid grid-cols-2 items-center gap-10'>
+                {/* author content */}
+                <div className="text-center">
+                  <h1 className="text-2xl font-bold">FEATURED AUTHORS</h1>
+                <h2 className="text-xl">Captivates with Every Word</h2>
+                <p className='text-justify mt-9'>passionate writer, avid reader, and lifelong explorer of ideas. My journey as an author began with a simple thought—stories have the power to connect hearts, spark curiosity, and leave lasting impressions. Over the years, I’ve dedicated myself to creating books that inspire, entertain, and encourage readers of all ages to dream beyond boundaries.</p>
+
+                <p className='text-justify mt-5'>My writing style blends emotion, creativity, and meaningful themes, often inspired by everyday moments, human experiences, and the beauty of possibility. Whether I’m crafting heartfelt characters, exploring mysterious plots, or sharing real-world insights, my goal remains the same: to give readers a story worth remembering</p>  
+                </div>
+                {/* author image */}
+                <div className="p-5 flex justify-center items-center">
+                    <img className='w-full' src="https://images.squarespace-cdn.com/content/v1/6091c3e4f5f6071721c43f77/8722ad5e-1398-445d-afdb-30c7bd402a59/DSC06581.jpg" alt="author" />
+                </div>
+                </section>
             {/* testimony */}
+            <section className='md:px-40 p-5 my-5 flex flex-col justify-center items-center'>
+                <h1 className="text-3xl font0bold">TESTIMONIALS</h1>
+                <h2 className="text-2xl">See What Others Are Saying</h2>
+                <div className="my-5 flex justify-center items-center flex-col">
+                    {/* user image */}
+                    <img width={'200px'} height={'200px'} style={{borderRadius:'50%'}}  src="https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg" alt="user" />
+                    {/* feedback */}
+                    <p className='mt-5'>Lucas Alexander</p>
+                    <p className='text-justify mt-4'><span className='font-bold me-2'>"Amazing Collection and Easy Navigation!"</span>"I love how simple and smooth the shopping experience is. The categories, reviews, and recommendations helped me find exactly what I was looking for. I'll definitely be back for more books! My order arrived earlier than expected and the packaging was perfect. The books were in excellent condition, and I even got a discount on my purchase. Highly recommended!"</p>
+                </div>
+            </section>
         </div>
         <Footer/>
         </>
