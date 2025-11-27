248d8eaac74ac3e418d0ef555975eb36bed093d4
Athulya-2524 <athulyasubash02@gmail.com>
2025-11-27 23:37:35 +0530

    view pages completed

--- src/users/pages/Contact.jsx
+++ src/users/pages/Contact.jsx
@@ -5,7 +5,9 @@ import Footer from '../../components/Footer';
 
 function Contacts() {
     return (
-    
+      <>
+       <Header/>
+
          <div className="w-full py-10 px-4 md:px-16 lg:px-28">
       {/* Heading */}
       <h2 className="text-center text-3xl font-bold">Contacts</h2>
@@ -75,7 +77,9 @@ function Contacts() {
 
       </div>
     </div>
-    
+
+    <Footer/>
+    </>
     )
     
 }
